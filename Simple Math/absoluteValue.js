df5ba7ac0532eacda3475634e74ae4288facfab6
nnhhiilliisstt <kumar.aditya221b@email.com>
2020-09-26 08:57:56 +0530

    added absolute value finding algorithm

---
+++ Simple Math/absoluteValue.js
@@ -0,0 +1,14 @@
+//find the absolute value of a number.
+
+function absoluteValue(num) {
+  // Find absolute value of `num`
+  if (num < 0) {
+    return -num;
+  }
+  // Executes if condition is not met
+  return num;
+}
+
+// Run `absoluteValue` function to find absolute value of two numbers.
+
+console.log(absoluteValue(-7689));
