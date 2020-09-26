259334c9c9674b91bdf40c6dc7b00b0d4110cd6f
nnhhiilliisstt <kumar.aditya221b@email.com>
2020-09-26 09:14:33 +0530

    finding average of an array of numbers

---
+++ Simple Math/mean.js
@@ -0,0 +1,18 @@
+//find the average (mean) of an array of numbers
+
+function mean(nums) {
+  var sum = 0;
+  var avg;
+
+  // This loop sums all values in the 'nums' array.
+  nums.forEach(function (current) {
+    sum += current;
+  });
+
+  // Divide sum by the length of the 'nums' array.
+  avg = sum / nums.length;
+  return avg;
+}
+
+// Run `mean` Function to find average of a list of numbers.
+console.log(mean([2, 4, 6, 8, 20, 50, 70]));
